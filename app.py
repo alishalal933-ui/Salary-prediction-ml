151cdb8c43702f2847ea1afdbba91fcd28418494
alishalal933-ui <alishalal933@gmail.com>
2026-07-07 10:21:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 import joblib
-import numpy as np
 
-model = joblib.load('salary_model.pkl')
+# Yahan apna CSV ka naam likho
+df = pd.read_csv('salary_data.csv') 
 
-st.title("💰 Salary Prediction App")
-st.write("Exp aur Age daal kar salary predict karo")
+X = df[['Experience', 'Age']]  # 2 features
+y = df['Salary']
 
-exp = st.number_input("Experience in Years", min_value=0, max_value=50, value=5)
-age = st.number_input("Age", min_value=18, max_value=70, value=25)
+model = LinearRegression()
+model.fit(X, y)
 
-if st.button("Predict Salary"):
-    input_data = np.array([[exp, age]])
-    prediction = model.predict(input_data)
-    st.success(f"Predicted Salary: Rs {prediction[0]:,.2f}")
+joblib.dump(model, 'salary_model.pkl')
+print("Model ban gaya dono features ke saath")
