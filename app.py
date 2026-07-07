1811b518f40db07fb310e50cb9b7dcd856bde2cb
alishalal933-ui <alishalal933@gmail.com>
2026-07-07 10:12:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ import numpy as np
 model = joblib.load('salary_model.pkl')
 
 st.title("Salary App")
+
+# Model ko kitne features chahiye ye pata karo
+n_features = model.n_features_in_
+st.write(f"Model expects {n_features} inputs")
+
 exp = st.number_input("Exp")
-age = st.number_input("Age") # 2nd feature add kar diya
+age = st.number_input("Age")
 
 if st.button("Predict"):
-    input_data = np.array([[exp, age]]) # 2 features bhej diye
+    if n_features == 1:
+        input_data = np.array([[exp]])
+    else:
+        input_data = np.array([[exp, age]]) # 2 features bhej do
+    
     prediction = model.predict(input_data)
     st.write(f"Predicted Salary: {prediction[0]:.2f}")
