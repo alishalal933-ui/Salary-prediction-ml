d5156542d4ca303e2b836222f51031ebcd99e1d7
alishalal933-ui <alishalal933@gmail.com>
2026-07-07 10:08:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ model = joblib.load('salary_model.pkl')
 
 st.title("Salary App")
 exp = st.number_input("Exp")
+age = st.number_input("Age") # 2nd feature add kar diya
+
 if st.button("Predict"):
-    input_data = np.array([[exp]])
+    input_data = np.array([[exp, age]]) # 2 features bhej diye
     prediction = model.predict(input_data)
-    st.write(f"Predicted Salary: {prediction[0]}")
+    st.write(f"Predicted Salary: {prediction[0]:.2f}")
