492f68c8cd298f8d0857763ee89c9a87a9eb6c4a
alishalal933-ui <alishalal933@gmail.com>
2026-07-07 10:02:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import joblib
+import numpy as np
 
 model = joblib.load('salary_model.pkl')
 
 st.title("Salary App")
 exp = st.number_input("Exp")
 if st.button("Predict"):
-    st.write(model.predict([[exp]]))
+    input_data = np.array([[exp]])
+    prediction = model.predict(input_data)
+    st.write(f"Predicted Salary: {prediction[0]}")
